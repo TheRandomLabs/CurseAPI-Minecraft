ce0b15feaee674326ffa3dea93cfbd07b6101518
TheRandomLabs <therandomlabsinc@gmail.com>
2018-01-19 22:20:43 +1100

    Update LICENSE

--- src/main/java/com/therandomlabs/curseapi/minecraft/Mod.java
+++ src/main/java/com/therandomlabs/curseapi/minecraft/Mod.java
@@ -3,8 +3,9 @@ package com.therandomlabs.curseapi.minecraft;
 import com.therandomlabs.curseapi.CurseException;
 import com.therandomlabs.curseapi.CurseProject;
 import com.therandomlabs.curseapi.util.CloneException;
+import com.therandomlabs.utils.throwable.ThrowableHandling;
 
-public class Mod implements Cloneable {
+public class Mod implements Cloneable, Comparable<Mod> {
 	public static final String UNKNOWN_NAME = "Unknown Name";
 
 	public String title = UNKNOWN_NAME;
@@ -28,6 +29,27 @@ public class Mod implements Cloneable {
 		mod.group = group;
 
 		return mod;
+	}
+
+	@Override
+	public int compareTo(Mod mod) {
+		if(title.equals(UNKNOWN_NAME)) {
+			try {
+				title = CurseProject.fromID(projectID).title();
+			} catch(CurseException ex) {
+				ThrowableHandling.handle(ex);
+			}
+		}
+
+		if(mod.title.equals(UNKNOWN_NAME)) {
+			try {
+				mod.title = CurseProject.fromID(mod.projectID).title();
+			} catch(CurseException ex) {
+				ThrowableHandling.handle(ex);
+			}
+		}
+
+		return title.compareTo(mod.title);
 	}
 
 	public String title() throws CurseException {
