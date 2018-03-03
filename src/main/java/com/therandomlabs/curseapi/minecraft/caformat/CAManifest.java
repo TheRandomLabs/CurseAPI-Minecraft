6d7bd1eebf7f90d2e1fc7a1941f29e0641fdf5a3
TheRandomLabs <therandomlabsinc@gmail.com>
2018-03-03 17:37:33 +1100

    Whoops

--- src/main/java/com/therandomlabs/curseapi/minecraft/caformat/CAManifest.java
+++ src/main/java/com/therandomlabs/curseapi/minecraft/caformat/CAManifest.java
@@ -427,7 +427,7 @@ public class CAManifest {
 		final String path = StringUtils.replaceAll(ArrayUtils.join(data, " "),
 				SPACE_PLACEHOLDER, ' ');
 
-		if(!IOUtils.isValidPath(path.replaceAll("*", "tmp").replaceAll("?", "tmp"))) {
+		if(!IOUtils.isValidPath(path.replaceAll("\\*", "tmp").replaceAll("\\?", "tmp"))) {
 			throw new ManifestParseException("Invalid path: " + path);
 		}
 
@@ -465,7 +465,7 @@ public class CAManifest {
 
 			element = StringUtils.replaceAll(element, SPACE_PLACEHOLDER, ' ');
 
-			if(!IOUtils.isValidPath(element.replaceAll("*", "tmp").replaceAll("?", "tmp"))) {
+			if(!IOUtils.isValidPath(element.replaceAll("\\*", "tmp").replaceAll("\\?", "tmp"))) {
 				throw new ManifestParseException("Invalid path: " + element);
 			}
 
