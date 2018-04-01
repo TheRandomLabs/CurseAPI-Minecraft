aa720d71bd760cd150f69e79a6a0e2d09307b55d
TheRandomLabs <therandomlabsinc@gmail.com>
2018-04-01 14:18:55 +1000

    Minor reformat

--- src/main/java/com/therandomlabs/curseapi/minecraft/modpack/manifest/CurseManifest.java
+++ src/main/java/com/therandomlabs/curseapi/minecraft/modpack/manifest/CurseManifest.java
@@ -13,72 +13,6 @@ import com.therandomlabs.utils.concurrent.ThreadUtils;
 import com.therandomlabs.utils.misc.StringUtils;
 
 public final class CurseManifest implements Cloneable, Serializable {
-	private static final long serialVersionUID = -8163938330549340465L;
-
-	public String manifestType = "minecraftModpack";
-	public int manifestVersion = 1;
-	public String name;
-	public String version;
-	public String author;
-	public String description;
-	public CurseMod[] files;
-	public String overrides = "Overrides";
-	public MinecraftInfo minecraft;
-	public int projectID;
-
-	public ExtendedCurseManifest toExtendedManifest() throws CurseException {
-		final ExtendedCurseManifest manifest = new ExtendedCurseManifest();
-
-		manifest.manifestType = manifestType;
-		manifest.manifestVersion = manifestVersion;
-		manifest.id = StringUtils.replaceWhitespace(name.toLowerCase(Locale.ROOT), "_");
-		manifest.name = name;
-		manifest.version = version;
-		manifest.author = author;
-		manifest.description = description;
-		manifest.files = CurseMod.toMods(files);
-		manifest.overrides = overrides;
-		manifest.minecraft = minecraft.clone();
-		manifest.projectID = projectID;
-
-		return manifest;
-	}
-
-	public String toJson() {
-		return new Gson().toJson(this);
-	}
-
-	public String toPrettyJsonWithTabs() {
-		return toPrettyJson().replaceAll(" {2}", "\t");
-	}
-
-	@Override
-	public CurseManifest clone() {
-		try {
-			final CurseManifest manifest = (CurseManifest) super.clone();
-
-			manifest.files = CloneException.tryClone(files);
-			manifest.minecraft = minecraft.clone();
-
-			return manifest;
-		} catch(CloneNotSupportedException ignored) {}
-
-		return null;
-	}
-
-	public String toPrettyJson() {
-		return new GsonBuilder().setPrettyPrinting().create().toJson(this);
-	}
-
-	public boolean containsMod(int projectID, int fileID) {
-		for(CurseMod mod : files) {
-			if(mod.projectID == projectID && mod.fileID == fileID) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 	public static class CurseMod implements Cloneable, Serializable {
 		private static final long serialVersionUID = -6936293567291965636L;
 
@@ -147,5 +81,71 @@ public final class CurseManifest implements Cloneable, Serializable {
 		public static CurseMod fromMod(Mod mod) {
 			return new CurseMod(mod.projectID, mod.fileID, mod.required);
 		}
+	}
+
+	private static final long serialVersionUID = -8163938330549340465L;
+
+	public String manifestType = "minecraftModpack";
+	public int manifestVersion = 1;
+	public String name;
+	public String version;
+	public String author;
+	public String description;
+	public CurseMod[] files;
+	public String overrides = "Overrides";
+	public MinecraftInfo minecraft;
+	public int projectID;
+
+	public ExtendedCurseManifest toExtendedManifest() throws CurseException {
+		final ExtendedCurseManifest manifest = new ExtendedCurseManifest();
+
+		manifest.manifestType = manifestType;
+		manifest.manifestVersion = manifestVersion;
+		manifest.id = StringUtils.replaceWhitespace(name.toLowerCase(Locale.ROOT), "_");
+		manifest.name = name;
+		manifest.version = version;
+		manifest.author = author;
+		manifest.description = description;
+		manifest.files = CurseMod.toMods(files);
+		manifest.overrides = overrides;
+		manifest.minecraft = minecraft.clone();
+		manifest.projectID = projectID;
+
+		return manifest;
+	}
+
+	public String toJson() {
+		return new Gson().toJson(this);
+	}
+
+	public String toPrettyJsonWithTabs() {
+		return toPrettyJson().replaceAll(" {2}", "\t");
+	}
+
+	@Override
+	public CurseManifest clone() {
+		try {
+			final CurseManifest manifest = (CurseManifest) super.clone();
+
+			manifest.files = CloneException.tryClone(files);
+			manifest.minecraft = minecraft.clone();
+
+			return manifest;
+		} catch(CloneNotSupportedException ignored) {}
+
+		return null;
+	}
+
+	public String toPrettyJson() {
+		return new GsonBuilder().setPrettyPrinting().create().toJson(this);
+	}
+
+	public boolean containsMod(int projectID, int fileID) {
+		for(CurseMod mod : files) {
+			if(mod.projectID == projectID && mod.fileID == fileID) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
