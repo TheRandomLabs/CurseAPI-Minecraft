051cfc048033cd39e9d32f948d22fc90d18a5160
TheRandomLabs <therandomlabsinc@gmail.com>
2018-04-02 21:46:49 +1000

    Move Light/DarkChocolate constants

--- src/main/java/com/therandomlabs/curseapi/minecraft/CurseAPIMinecraft.java
+++ src/main/java/com/therandomlabs/curseapi/minecraft/CurseAPIMinecraft.java
@@ -1,11 +1,44 @@
 package com.therandomlabs.curseapi.minecraft;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import com.therandomlabs.curseapi.minecraft.forge.MinecraftForge;
 
 public final class CurseAPIMinecraft {
+	public static final int LIGHTCHOCOLATE_PROJECT_ID = 257165;
+	public static final String LIGHTCHOCOLATE_ID = "lightchocolate";
+
+	public static final String LIGHTCHOCOLATE_REPO =
+			"https://github.com/TheRandomLabs/LightChocolate";
+	public static final URL LIGHTCHOCOLATE_REPO_URL = url(LIGHTCHOCOLATE_REPO);
+	public static final String LIGHTCHOCOLATE_DOWNLOAD =
+			LIGHTCHOCOLATE_REPO + "/archive/master.zip";
+	public static final URL LIGHTCHOCOLATE_DOWNLOAD_URL = url(LIGHTCHOCOLATE_DOWNLOAD);
+	public static final String LIGHTCHOCOLATE_MANIFEST_TXT =
+			"https://raw.githubusercontent.com/TheRandomLabs/LightChocolate/master/manifest.txt";
+	public static final URL LIGHTCHOCOLATE_MANIFEST_TXT_URL = url(LIGHTCHOCOLATE_MANIFEST_TXT);
+	public static final String LIGHTCHOCOLATE_MANIFEST_JSON =
+			"https://raw.githubusercontent.com/TheRandomLabs/LightChocolate/master/manifest.json";
+	public static final URL LIGHTCHOCOLATE_MANIFEST_JSON_URL = url(LIGHTCHOCOLATE_MANIFEST_JSON);
+
+	public static final String DARKCHOCOLATE_ID = "darkchocolate";
+
+	public static final String DARKCHOCOLATE_MANIFEST_TXT = "https://raw.githubusercontent.com/" +
+			"TheRandomLabs/LightChocolate/master/darkchocolate.txt";
+	public static final URL DARKCHOCOLATE_MANIFEST_TXT_URL = url(DARKCHOCOLATE_MANIFEST_TXT);
+
 	private CurseAPIMinecraft() {}
 
 	public static void clearAllCache() {
 		MinecraftForge.clearChangelogCache();
+	}
+
+	private static URL url(String url) {
+		try {
+			return new URL(url);
+		} catch(MalformedURLException ignored) {
+			//This will never happen unless I screw up the URLs somehow
+		}
+		return null;
 	}
 }
--- src/main/java/com/therandomlabs/curseapi/minecraft/caformat/pre/PreprocessorImport.java
+++ src/main/java/com/therandomlabs/curseapi/minecraft/caformat/pre/PreprocessorImport.java
@@ -4,25 +4,18 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 import java.util.List;
 import com.therandomlabs.curseapi.CurseException;
+import com.therandomlabs.curseapi.minecraft.CurseAPIMinecraft;
 import com.therandomlabs.curseapi.util.DocumentUtils;
 import com.therandomlabs.utils.collection.ImmutableList;
+import com.therandomlabs.utils.io.IOUtils;
 import com.therandomlabs.utils.misc.StringUtils;
+import com.therandomlabs.utils.network.NetworkUtils;
 
 public class PreprocessorImport extends Preprocessor {
 	public static final char CHARACTER = '#';
-
-	public static final String LIGHTCHOCOLATE = "lightchocolate";
-	public static final String LIGHTCHOCOLATE_URL =
-			"https://raw.githubusercontent.com/TheRandomLabs/LightChocolate/master/manifest.txt";
-
-	public static final String DARKCHOCOLATE = "darkchocolate";
-	public static final String DARKCHOCOLATE_URL =
-			"https://raw.githubusercontent.com/TheRandomLabs/LightChocolate/master/" +
-					"darkchocolate.txt";
 
 	PreprocessorImport() {}
 
@@ -33,32 +26,20 @@ public class PreprocessorImport extends Preprocessor {
 
 	@Override
 	public boolean isValid(String value, String[] args) {
-		if(value.equalsIgnoreCase(LIGHTCHOCOLATE) || value.equalsIgnoreCase(DARKCHOCOLATE)) {
-			return true;
-		}
-
-		try {
-			new URL(value);
-			return true;
-		} catch(MalformedURLException ex) {}
-
-		try {
-			Paths.get(value);
-			return true;
-		} catch(InvalidPathException ex) {}
-
-		return false;
+		return value.equalsIgnoreCase(CurseAPIMinecraft.LIGHTCHOCOLATE_ID) ||
+				value.equalsIgnoreCase(CurseAPIMinecraft.DARKCHOCOLATE_ID) ||
+				NetworkUtils.isValidURL(value) || IOUtils.isValidPath(value);
 	}
 
 	@Override
 	public List<String> apply(String value, String[] args) throws CurseException, IOException {
-		List<String> linesToImport = null;
+		List<String> linesToImport;
 
 		try {
-			if(value.equalsIgnoreCase(LIGHTCHOCOLATE)) {
-				value = LIGHTCHOCOLATE_URL;
-			} else if(value.equalsIgnoreCase(DARKCHOCOLATE)) {
-				value = DARKCHOCOLATE_URL;
+			if(value.equalsIgnoreCase(CurseAPIMinecraft.LIGHTCHOCOLATE_ID)) {
+				value = CurseAPIMinecraft.LIGHTCHOCOLATE_MANIFEST_TXT;
+			} else if(value.equalsIgnoreCase(CurseAPIMinecraft.DARKCHOCOLATE_ID)) {
+				value = CurseAPIMinecraft.DARKCHOCOLATE_MANIFEST_TXT;
 			}
 
 			final URL url = new URL(value);
